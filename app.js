fdb711238a9e0ff27f4e4b55c325412079c1cfb9
wesleyforrest <79322205+wesleyforrest@users.noreply.github.com>
2022-05-19 16:07:00 +0100

    Added link to app.js, button ids and additional classes for numbers and operator buttons.  Also created a variable to update number in calc display

--- app.js
+++ app.js
@@ -1,3 +1,29 @@
-//Create variables for calculator
+//Create variable to update number in display
+
+const displayNumber = document.querySelector("#display-number");
+displayNumber.innerHTML = "0";
+
+// create function to select each button individually
+
+const buttonValues = [
+  0,
+  1,
+  2,
+  3,
+  4,
+  5,
+  6,
+  7,
+  8,
+  9,
+  "+",
+  "-",
+  "*",
+  "/",
+  "=",
+  "ac",
+];
+
+const buttonSelector = buttonValues.forEach((element) => {});
 
 //Use an array to push number to then "Reduce" for any calculation using add only
