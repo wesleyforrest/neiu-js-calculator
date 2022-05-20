f9d9e00727b96086dac935a35a0ba12a83792c34
wesleyforrest <79322205+wesleyforrest@users.noreply.github.com>
2022-05-20 16:08:20 +0100

    Added comments to explain code blocks written so far and sudo code for logic to carry out calculations for simple sums using +,-,* and /

--- app.js
+++ app.js
@@ -1,39 +1,44 @@
-//Create variable to update number in display
-
+// Create variables to target numbers and operators by class (use querySelectorAll for numbers and operators, QuerySelector for "AC" button)
 let numbers = document.querySelectorAll(".number");
 let operators = document.querySelectorAll(".operator");
-const displayNumber = document.querySelector("#display-number");
+
+//Target "AC" button seperatly as output is not required is to clear display, not output characters to display
 const clearButton = document.querySelector("#btn-ac");
+
+//Create variable to update display output from calculator keyboard
+const displayNumber = document.querySelector("#display-number");
+
+//Create function to display numbers in display when clicked (use forEach to map each number to an event listener)
+const showNumbers = (event) => {
+  displayNumber.innerHTML += event.target.innerText;
+};
+numbers.forEach((number) => {
+  number.addEventListener("click", showNumbers);
+});
+//Create function to display operators in display when clicked (use forEach to map each operator to an event listener)
+const showOperators = (event) => {
+  displayNumber.innerHTML = event.target.innerText;
+};
+operators.forEach((operator) => {
+  operator.addEventListener("click", showOperators);
+});
+//Create function to clear display when "AC" button clicked
+const clearAll = () => {
+  displayNumber.innerHTML = "";
+};
+clearButton.addEventListener("click", clearAll);
+displayNumber.innerHTML = "";
+
+//Create empty variables for first & second input, operator, stored number (generated from first number once operator button clicked) and answer (generated once calculation (=) button is clicked)
 let firstNumber = "";
 let secondNumber = "";
 let operatorButton = "";
 let storedNumber = "";
-const clearAll = () => {
-  displayNumber.innerHTML = "";
-};
+let answer = "";
 
-const showNumbers = (event) => {
-  displayNumber.innerHTML += event.target.innerText;
-};
+//Use an array to push number to then "Reduce" for any calculation using add only??
 
-const showOperators = (event) => {
-  displayNumber.innerHTML = event.target.innerText;
-};
-
-clearButton.addEventListener("click", clearAll);
-displayNumber.innerHTML = "";
-
-numbers.forEach((number) => {
-  number.addEventListener("click", showNumbers);
-});
-
-operators.forEach((operator) => {
-  operator.addEventListener("click", showOperators);
-});
-// create function to select each button individually
-
-//Use an array to push number to then "Reduce" for any calculation using add only
-
+//Create a switch statement to handle inputs and carry out calculation required depending on operator used
 sum.addEventListener("click", () => {
   let answer;
   switch (oper) {
